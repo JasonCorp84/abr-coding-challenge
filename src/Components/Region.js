8239f40af30b9e02888a4ca0d80b8d9f4768888d
Csaba Balogh <csababalogh84@gmail.com>
2024-10-05 18:26:25 -0700

    abr coding challenge fix padding

--- src/Components/Region.js
+++ src/Components/Region.js
@@ -7,18 +7,18 @@ const Region = ({region, regionData }) => {
   const averageCalories = calculateAverageCalories(regionData, 'Calories');
   const averageTotalFat = calculateAverageCalories(regionData, 'FatTotal')
   return (
-    <div >
+    <div style={{padding: '3%'}}>
       <h2>{region} Region</h2>
       <p><strong>Average Calories</strong> per Serving in the {region} Region: {averageCalories}</p>
       <p><strong>Average Total Fat</strong> per Serving in the {region} region: {averageTotalFat}</p>
       <div className='region-container'>
-      {regionData.map(fish => {     
-        const {SpeciesName} = fish;
-        return (
-            <FishItem key={SpeciesName} fishInfo={fish}/>
-          )
-      }
-       )}
+        {regionData.map(fish => {     
+          const {SpeciesName} = fish;
+          return (
+              <FishItem key={SpeciesName} fishInfo={fish}/>
+            )
+        }
+        )}
       </div>
     </div>
   );
